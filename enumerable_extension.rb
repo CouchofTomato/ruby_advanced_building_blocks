2d1500b0e8558c88da8712875d451530173f95cf
Austin P Mason <austinpmason@gmail.com>
2015-10-15 14:30:30 +0100

    Include my_select method

--- enumerable_extension.rb
+++ enumerable_extension.rb
@@ -18,81 +18,28 @@ module Enumerable
 	end
 
 	def my_select
+		return enum_for(:each) unless block_given?
 		return_array = []
-		self.my_each do |n|
-			result << n if yield n
+		self.my_each do |input|
+			return_array << input if yield input 
 		end
 		return_array
-	end
-
-	def my_all?
-		self.my_each do |n|
-			is_true = yield n
-			if is_true == false
-				return false
-			end
-		end
-		true
-	end
-
-	def my_any?
-		self.my_each do |n|
-			is_true = yield n
-			if is_true
-				return true
-			end
-		end
-		false
-	end
-
-	def my_none?
-		self.my_each do |n|
-			is_true = yield n
-			if !is_true
-				return true
-			end
-		end
-		false
-	end
-
-	def my_count(*num)
-		count = 0
-		unless block_given?			
-			if args.empty?
-				self.my_each { |item| count += 1 }
-			else			
-				self.my_each { |item| count += 1 if item == args[0] }
-			end
-		else
-			self.my_each { |item| count += 1 if yield item }
-		end
-		count
-	end
-
-	def my_map
-		return_array = []
-		return self unless block_given?
-		self.my_each do |n|
-			temp = yield n
-			return_array << temp
-		end
-		return_array
-	end
-
-	def my_inject(num = nil)
-		sum = num || self.first
-		unless block_given?
-			sum = self.to_a
-		else
-			self.to_a.my_each { |n|	sum = yield(sum, n) }
-		end
-		sum
 	end
 
 end
 
-test = [1,2,3]
-puts test.my_inject {|sum, val| sum - val}
-puts test.inject {|sum, val| sum - val}
+my_array = [1,2,3,4,5,6,7,8,9,10]
 
-
+puts "my_each"
+my_array.my_each { |num| print "#{num} "}
+puts
+puts
+puts "my_each_with_index"
+my_array.my_each_with_index { |num, idx| print "#{idx}: #{num} -- "}
+puts
+puts
+puts "my_select"
+print my_array.my_select { |num| num % 2 == 0}
+puts
+p my_array.my_select
+puts
